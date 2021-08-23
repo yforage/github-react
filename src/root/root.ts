e1876f0de99352e439d89b8fe5394d5cfc7857b4
yforage <yomuim@gmail.com>
2021-08-23 09:30:58 +0300

    place all in start function

--- src/root/root.ts
+++ src/root/root.ts
@@ -1,27 +1,31 @@
 // создание и использование GitHubStore
 import GitHubStore from '../store/GitHubStore';
 
-const api = new GitHubStore();
-const orgName = 'ktsstudio';
+const start = () => {
+  const api = new GitHubStore();
+  const orgName = 'ktsstudio';
 
-const getReposParams = {
-  orgName,
-  sort: 'updated',
-  type: 'public',
+  const getReposParams = {
+    orgName,
+    sort: 'updated',
+    type: 'public',
+  };
+
+  api.getRepoList(getReposParams).then((result) => {
+    console.log(result);
+  });
+
+  const addRepoParams = {
+    orgName,
+    name: 'kts-frontend',
+    description: 'My new repo for project',
+    private: true,
+    has_projects: false,
+  };
+
+  api.postCreateRepo(addRepoParams).then((result) => {
+    console.log(result);
+  });
 };
 
-api.getRepoList(getReposParams).then((result) => {
-  console.log(result);
-});
-
-const addRepoParams = {
-  orgName,
-  name: 'kts-frontend',
-  description: 'My new repo for project',
-  private: true,
-  has_projects: false,
-};
-
-api.postCreateRepo(addRepoParams).then((result) => {
-  console.log(result);
-});
+export default start;
