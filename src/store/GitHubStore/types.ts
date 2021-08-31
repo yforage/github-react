6af77962c5a7bc4567e30b192a98c19df6f6a5d8
yforage <yomuim@gmail.com>
2021-08-31 14:21:49 +0300

    types for branches request

--- src/store/GitHubStore/types.ts
+++ src/store/GitHubStore/types.ts
@@ -28,6 +28,13 @@ export type GetRepoListParams = {
   page?: number;
 };
 
+export type GetRepoBranchesParams = {
+  owner: string;
+  repo: string;
+  per_page?: number;
+  page?: number;
+};
+
 type RepoOwner = {
   id: number;
   login: string;
@@ -41,11 +48,19 @@ export type RepoItem = {
   url: string;
   stargazers_count: number;
   owner: RepoOwner;
+  updated_at: string;
+};
+
+export type BranchesItem = {
+  name: string;
 };
 
 export interface IGitHubStore {
   getRepoList(params: GetRepoListParams): Promise<ApiResponse<RepoItem[], {}>>;
 
+  getRepoBranches(
+    params: GetRepoBranchesParams
+  ): Promise<ApiResponse<BranchesItem[], {}>>;
   // Необязательный пункт, т.к. требует авторизации. Понадобится в будущем
   postCreateRepo(
     params: PostCreateRepoParams
