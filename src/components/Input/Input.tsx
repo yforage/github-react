72403c8edd00c0e0f6940f1f580822ba19af0f25
yforage <yomuim@gmail.com>
2021-09-07 08:27:04 +0300

    return memoized component, fix typo

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 type InputProps = {
   value: string;
-  placeHolder: string;
+  placeholder: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Input: React.FC<InputProps> = ({ value, placeHolder, onChange }) => {
+const Input: React.FC<InputProps> = ({ value, placeholder, onChange }) => {
   return (
     <div className="git-repo-search__input-container">
       <input
         type="text"
         name="repo_name"
-        placeholder={placeHolder}
+        placeholder={placeholder}
         className="git-repo-search__input"
         onChange={onChange}
         value={value}
@@ -21,4 +21,4 @@ const Input: React.FC<InputProps> = ({ value, placeHolder, onChange }) => {
   );
 };
 
-export default Input;
+export default React.memo(Input);
