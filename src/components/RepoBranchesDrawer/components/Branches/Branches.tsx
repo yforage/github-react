a0882684808f1ef5c531af53f031a1bf55ee9128
yforage <yomuim@gmail.com>
2021-09-16 18:16:37 +0300

    replace string endpoints, add alias

--- src/components/RepoBranchesDrawer/components/Branches/Branches.tsx
+++ src/components/RepoBranchesDrawer/components/Branches/Branches.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 
+import apiEndpoints from "@config/api";
+import GitHubStore from "@store/GitHubStore";
 import { BranchesItem, RepoInfoItem } from "src/store/GitHubStore/types";
 
-import GitHubStore from "../../../../store/GitHubStore";
 import styles from "./Branches.module.scss";
 import BranchItem from "./components/BranchItem";
 
@@ -26,11 +27,16 @@ const Branches: React.FC<BranchesProps> = ({ repo }) => {
     })();
     return () => handleChange([]);
   }, [repo]);
+  if (!repo) return null;
   return (
     <div className={styles.branches}>
       <p className={styles.branches__title}>Branches:</p>
       {branchesList.map((branch) => {
-        const href = `https://github.com/${repo?.owner.login}/${repo?.name}/tree/${branch.name}`;
+        const href = apiEndpoints.branchLink(
+          repo.owner.login,
+          repo.name,
+          branch.name
+        );
         return <BranchItem key={branch.name} name={branch.name} href={href} />;
       })}
     </div>
