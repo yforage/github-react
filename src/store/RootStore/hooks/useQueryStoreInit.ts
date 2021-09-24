38933f41864a00a4a22c2be41204bfd8d3947475
yforage <yomuim@gmail.com>
2021-09-24 11:38:52 +0300

    hook-connector for query store

---
+++ src/store/RootStore/hooks/useQueryStoreInit.ts
@@ -0,0 +1,10 @@
+import { useLocation } from "react-router";
+import { useHistory } from "react-router-dom";
+
+import rootStore from "../instance";
+
+export const useQueryStoreInit = (): void => {
+  const location = useLocation();
+  const history = useHistory();
+  rootStore.query.setHistory(history, location);
+};
