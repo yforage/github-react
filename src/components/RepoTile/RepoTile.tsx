75eb7560934ab5ace0cfc7bd3189555d57a6a12b
yforage <yomuim@gmail.com>
2021-09-24 11:26:01 +0300

    snake case to camel case

--- src/components/RepoTile/RepoTile.tsx
+++ src/components/RepoTile/RepoTile.tsx
@@ -2,28 +2,28 @@ import React from "react";
 
 import Avatar from "@components/Avatar";
 import StarIcon from "@components/StarIcon";
-import { RepoItem } from "@store/GitHubStore/types";
+import { RepoItemModel } from "@store/models/gitHub";
 import prettyDate from "@utils/prettyDate";
 
 import styles from "./RepoTile.module.scss";
 
 type RepoTileProps = {
-  item: RepoItem;
+  item: RepoItemModel;
 };
 
 const RepoTile: React.FC<RepoTileProps> = ({ item }) => {
-  const updatedDate = prettyDate(item.updated_at);
+  const updatedDate = prettyDate(item.updatedAt);
   const numberFormat = Intl.NumberFormat("en", { notation: "compact" });
   return (
     <div className={styles.repoTile}>
-      <Avatar letter={item.name[0]} src={item.owner.avatar_url} />
+      <Avatar letter={item.name[0]} src={item.owner.avatarUrl} />
       <div className={styles.repoTile__content}>
         <p className={styles.repoTile__repoName}>{item.name}</p>
         <span className={styles.repoTile__orgName}>{item.owner.login}</span>
         <div className={styles.repoTile__info}>
           <div className={styles.repoTile__stars}>
             <StarIcon />{" "}
-            <span>{numberFormat.format(item.stargazers_count)}</span>
+            <span>{numberFormat.format(item.stargazersCount)}</span>
           </div>
           <div>
             <span>Updated {updatedDate}</span>
