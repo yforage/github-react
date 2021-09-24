f371a9f286090a9c62e06451cb70d9c1e53afe94
yforage <yomuim@gmail.com>
2021-09-24 11:21:02 +0300

    move browserrouter from app

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+
 import "./styles/index.scss";
+import "./config/configureMobx.ts";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
