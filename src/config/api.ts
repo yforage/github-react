b6eed19f3c80b049341ea69b5e80ea94ed7dd302
yforage <yomuim@gmail.com>
2021-09-24 11:26:19 +0300

    remove link creator

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,4 @@
 const apiEndpoints = {
-  branchLink: (owner: string, repoName: string, branchName: string): string =>
-    `https://github.com/${owner}/${repoName}/tree/${branchName}`,
   orgRepos: (org: string): string => `/orgs/${org}/repos`,
   repoBranches: (owner: string, repo: string): string =>
     `/repos/${owner}/${repo}/branches`,
